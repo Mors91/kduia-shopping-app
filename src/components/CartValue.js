ae053b52bbb3bdde5b28ef594db05e62f242358d
whytemors <snrwhyte91@yahoo.com>
2023-11-08 03:02:55 -0500

    1

--- src/components/CartValue.js
+++ src/components/CartValue.js
@@ -1,17 +1,37 @@
 import React, { useContext } from 'react';
+import { Formik, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
 import { AppContext } from '../context/AppContext';
 
 const CartValue = () => {
     const { expenses, Location } = useContext(AppContext);
+    const validationSchema = Yup.object().shape({
+        numberField: Yup.number()
+          .typeError('Please enter a valid number')
+          .required('This field is required')
+      });
     const totalExpenses = expenses.reduce((total, item) => {
-        return (total += (item.unitprice * item.quantity));
+      return (total += (item.unitprice * item.quantity));
     }, 2000);
-
+  
     return (
-        <div className='alert alert-primary'>
+      <Formik
+        initialValues={{ numberField: '' }}
+        validationSchema={validationSchema}
+        //onSubmit={handleSubmit}
+      >
+        {({ handleSubmit }) => (
+          <div className='alert alert-primary'>
             <span>Budget: {Location}{totalExpenses}</span>
-        </div>
+            <form onSubmit={handleSubmit}>
+              <Field type='text' name='numberField' />
+              <ErrorMessage name='numberField' component='div' />
+              <button type='submit'>Submit</button>
+            </form>
+          </div>
+        )}
+      </Formik>
     );
-};
+  };
 
-export default CartValue;
+  export default CartValue;
