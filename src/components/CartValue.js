38f00936c04a3ff27b9b62d01f5624d19af02882
whytemors <snrwhyte91@yahoo.com>
2023-11-14 14:43:35 -0500

    fixed validation

--- src/components/CartValue.js
+++ src/components/CartValue.js
@@ -1,37 +1,57 @@
 import React, { useContext } from 'react';
-import { Formik, Field, ErrorMessage } from 'formik';
+import { Formik, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 import { AppContext } from '../context/AppContext';
+import { InputNumber } from 'antd';
+import 'antd/dist/reset.css';
 
 const CartValue = () => {
-    const { expenses, Location } = useContext(AppContext);
-    const validationSchema = Yup.object().shape({
-        numberField: Yup.number()
-          .typeError('Please enter a valid number')
-          .required('This field is required')
-      });
-    const totalExpenses = expenses.reduce((total, item) => {
-      return (total += (item.unitprice * item.quantity));
-    }, 2000);
-  
-    return (
-      <Formik
-        initialValues={{ numberField: '' }}
-        validationSchema={validationSchema}
-        //onSubmit={handleSubmit}
-      >
-        {({ handleSubmit }) => (
-          <div className='alert alert-primary'>
-            <span>Budget: {Location}{totalExpenses}</span>
-            <form onSubmit={handleSubmit}>
-              <Field type='text' name='numberField' />
-              <ErrorMessage name='numberField' component='div' />
-              <button type='submit'>Submit</button>
-            </form>
-          </div>
-        )}
-      </Formik>
-    );
-  };
+  const { expenses, Location } = useContext(AppContext);
+  const validationSchema = Yup.object().shape({
+    numberField: Yup.number()
+      .typeError('Please enter a valid number')
+      .required('This field is required')
+      .max(20000, 'Value must be less than or equal to 20,000')
+      .test(
+        'is-increment-of-ten',
+        'Value must not exceed the upper limit by more than 10',
+        (value) => {
+          const upperLimit = 20000;
+          const margin = 10;
+          const diff = value - upperLimit;
+          return diff <= margin;
+        }
+      ),
+  });
 
-  export default CartValue;
+  const totalExpenses = expenses.reduce((total, item) => {
+    return (total += item.unitprice * item.quantity);
+  }, 2000);
+
+  return (
+    <Formik
+      initialValues={{ numberField: totalExpenses }}
+      validationSchema={validationSchema}
+      // onSubmit={handleSubmit}
+    >
+      {(formik) => (
+        <div className='alert alert-primary' style={{ display: 'flex', alignItems: 'center' }}>
+          <span style={{ marginRight: '10px' }}>Budget: {Location}</span>
+          <InputNumber
+            name='numberField'
+            value={formik.values.numberField}
+            onChange={(value) => formik.setFieldValue('numberField', value)}
+            formatter={(value) => ` ${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+            parser={(value) => value.replace(/\$\s?|(,*)/g, '')}
+            min={0}
+            max={20000}
+            step={10}
+          />
+          <ErrorMessage name='numberField' component='div' />
+        </div>
+      )}
+    </Formik>
+  );
+};
+
+export default CartValue;
